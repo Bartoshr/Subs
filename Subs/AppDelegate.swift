f0408c0af84d21e571beb2273d7aad6390b0fe52
Bartosh <bartoshx@gmail.com>
2016-03-12 18:38:58 +0100

    Sound of missing subtitles

--- Subs/AppDelegate.swift
+++ Subs/AppDelegate.swift
@@ -8,17 +8,80 @@
 
 import Cocoa
 import PathKit
-
+import Alamofire
+import AlamofireXMLRPC
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
     @IBOutlet weak var window: NSWindow!
     @IBOutlet weak var tableControler: TableControler!
+    
+    let userAgent =  "OSTestUserAgent";
+    let host = "https://api.opensubtitles.org:443/xml-rpc"
+    
+    var openSubtitles : OpenSubtitles
+    
+    var token : String = ""
+    var subtitles : [Subtitle] = []
+    
+    let emptySound = NSSound(named: "Hero")
+    
+
+    override init() {
+        openSubtitles = OpenSubtitles(host: host, userAgent: userAgent)
+    }
 
     func applicationDidFinishLaunching(aNotification: NSNotification) {
         // Insert code here to initialize your application
+        openSubtitles.logIn(logedIn);
+        tableControler.rowSelectedMethod = download
     }
+    
+    // Actions
+    
+    func search(path: String){
+        openSubtitles.searchSubtitles(self.token,
+            path: path,
+            properties: nil,
+            callback:searchComlpeted)
+    }
+    
+    func download(id: String){
+        let props = [id] as XMLRPCArray
+        openSubtitles.downloadSubtitles(token,
+            ids: props,
+            callback: downloadCompleted)
+    }
+    
+    // Callbacks
+    
+    func logedIn(token: String){
+        self.token = token
+        print(token)
+    }
+    
+    func searchComlpeted(data: [Subtitle])->Void {
+        
+        if data.count == 0 {
+            emptySound?.play()
+            return
+        }
+        
+        window.makeKeyAndOrderFront(self)
+        tableControler.subtitles = data
+        tableControler.tableView.reloadData()
+        
+    }
+    
+    
+    func downloadCompleted(data: [String])->Void {
+        let decodedData = NSData(base64EncodedString: data[0], options:NSDataBase64DecodingOptions(rawValue: 0))
+        let decompressedData : NSData = try! decodedData!.gunzippedData()
+        decompressedData.writeToFile(tableControler.directory!+"paczka.srt", atomically: true)
+    }
+    
+    // Application Handlers
 
     func applicationWillTerminate(aNotification: NSNotification) {
         // Insert code here to tear down your application
@@ -32,17 +95,14 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     func application(sender: NSApplication, openFiles filenames: [String]) {
         
         for filename in filenames {
-            
             let directory = Path(filename).parent()
             print(directory.description)
-            
-            tableControler.search(filename)
+            search(filename)
             tableControler.directory = directory.description+"/"
-            
-            window.makeKeyAndOrderFront(self)
         }
         
     }
+    
 
 }
 
--- Subs/TableControler.swift
+++ Subs/TableControler.swift
@@ -15,61 +15,10 @@ class TableControler: NSViewController, NSTableViewDataSource, NSTableViewDelega
     
     @IBOutlet weak var tableView: NSTableView!
     
-    let userAgent =  "OSTestUserAgent";
-    let host = "https://api.opensubtitles.org:443/xml-rpc"
-    
-    var token : String = ""
     var subtitles : [Subtitle] = []
-    var openSubtitles : OpenSubtitles
+    var directory: String?
     
-    var directory: String? = nil
-
-    required init?(coder: NSCoder) {
-        openSubtitles = OpenSubtitles(host: host, userAgent: userAgent)
-        super.init(coder: coder)
-    }
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        openSubtitles.logIn(logedIn);
-    }
-    
-    // Actions
-    
-    func search(path: String){
-        openSubtitles.searchSubtitles(self.token,
-            path: path,
-            properties: nil,
-            callback:searchComlpeted)
-    }
-    
-    func download(id: String){
-        let props = [id] as XMLRPCArray
-        openSubtitles.downloadSubtitles(token,
-            ids: props,
-            callback: downloadCompleted)
-    }
-    
-    
-    // Controler Life Cycle
-    
-    func logedIn(token: String){
-        self.token = token
-        print(token)
-    }
-    
-
-    func searchComlpeted(data: [Subtitle])->Void {
-        self.subtitles = data
-        self.tableView.reloadData()
-    }
-    
-    
-    func downloadCompleted(data: [String])->Void {
-        let decodedData = NSData(base64EncodedString: data[0], options:NSDataBase64DecodingOptions(rawValue: 0))
-        let decompressedData : NSData = try! decodedData!.gunzippedData()
-        decompressedData.writeToFile(directory!+"paczka.srt", atomically: true)
-    }
+    var rowSelectedMethod : ((String)->Void)?
     
     func numberOfRowsInTableView(aTableView: NSTableView) -> Int {
         return subtitles.count
@@ -96,7 +45,9 @@ class TableControler: NSViewController, NSTableViewDataSource, NSTableViewDelega
         if myTableViewFromNotification.numberOfSelectedRows != 0 {
             let indexes = myTableViewFromNotification.selectedRowIndexes
             let index = indexes.firstIndex
-            download(subtitles[index].idSubtitleFile)
+            
+            rowSelectedMethod!(subtitles[index].idSubtitleFile)
+            
             NSApplication.sharedApplication().keyWindow?.orderOut(self)
             NSApplication.sharedApplication().hide(self)
         }
