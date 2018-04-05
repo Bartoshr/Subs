34f3dee8b2c03ec708869f0c58f705302f27782f
Bartosz Ruminski <bartoshx@gmail.com>
2018-04-05 07:52:48 +0200

    Remove Pods Folder

--- Pods/Target Support Files/AlamofireXMLRPC/AlamofireXMLRPC-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_AlamofireXMLRPC : NSObject
-@end
-@implementation PodsDummy_AlamofireXMLRPC
-@end
--- Pods/Target Support Files/Pods-Subs/Pods-Subs-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Subs : NSObject
-@end
-@implementation PodsDummy_Pods_Subs
-@end
